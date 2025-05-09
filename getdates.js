1a3a7a909169eb4220e4f9e6c143ea1753d7652c
Codecamp-champ <aaron92coker@gmail.com>
2025-05-09 14:52:01 -0600

    Updates after reveiwing errors

--- getdates.js
+++ getdates.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
         currentYearSpan.textContent = new Date().getFullYear();
     }
 
-    // Get the last modified date and display it in the specified paragraph element
     const lastModifiedParagraph = document.getElementById('lastModified');
     if (lastModifiedParagraph) {
         lastModifiedParagraph.textContent = `Last Modified: ${document.lastModified}`;
